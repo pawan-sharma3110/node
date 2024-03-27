91f5f1e04d610d35937d7f73110b4ab1da4cbe06
pawan-sharma3110 <pawansharma93520@gmail.com>
2024-03-28 00:07:39 +0530

    learning handler

---
+++ index.js
@@ -0,0 +1,8 @@
+// let garnateName = require("sillyname");
+// import garnateName from "sillyname";
+// let sillyName = garnateName();
+// console.log(`My name is ${sillyName}`);
+
+import superheroes from "superheroes";
+const name = superheroes.random();
+console.log(`Your hero name is ${name}`);
